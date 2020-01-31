9f7bf7a1decdc6c2cafba7b65efce868d7704b52
DeeMeyers <justdmeyers@gmail.com>
2020-01-31 15:08:21 -0500

    learned how to spell 'listener'

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const allButtons = document.querySelectorAll(".button");
 const displayVal = document.querySelector(".display");
 console.log(allButtons);
@@ -5,24 +6,22 @@ console.log(displayVal);
 
 for(let eachButton of allButtons){ 
 
-    eachButton.addEventListner('click', function(){
+    eachButton.addEventListener('click', function(e){
             if(eachButton.value == 'C'){
                 //clear the display
                 displayVal.innerText = '';
-            };
+            }
             else if(eachButton.value == '='){
                 //eval display then push to display
-                displayVal.innerText = eval(displayVal.inerText)
-            };
+                displayVal.innerText = eval(displayVal.inerText);
+            }
             else{
                 //add button press to display string
-                displayVal.innerText = (displayVal.innerText + eachButton.innerText)
-            };
-        });
+                displayVal.innerText += eachButton.innerText;
+            }
+        }
+        );
     };
 
 
 
-// for(let eachButton of allButtons){
-
-// }
