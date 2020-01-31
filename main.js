b9bdeb843dbf10e0060c24e7b10f4768928cdfb6
DeeMeyers <justdmeyers@gmail.com>
2020-01-31 14:47:30 -0500

    calculator html/css right enough. working on js

--- main.js
+++ main.js
@@ -3,20 +3,26 @@ const displayVal = document.querySelector(".display");
 console.log(allButtons);
 console.log(displayVal);
 
-allButtons.addEventListner("click", function(e){
-    for (eachButton of allButtons){ 
-        if(eachButton.value == "C"){
-            //clear the display
-            displayVal.innerText = "";
-        }
-        else if(eachButton.value == "="){
-            //eval display then push to display
-            displayVal.innerText = eval(displayVal.inerText)
-        }
-        else{
-            //add button press to display string
-            displayVal.innerText = (displayVal.innerText + eachButton.innerText)
-        }
-    }
+for(let eachButton of allButtons){ 
 
-})
+    eachButton.addEventListner('click', function(){
+            if(eachButton.value == 'C'){
+                //clear the display
+                displayVal.innerText = '';
+            };
+            else if(eachButton.value == '='){
+                //eval display then push to display
+                displayVal.innerText = eval(displayVal.inerText)
+            };
+            else{
+                //add button press to display string
+                displayVal.innerText = (displayVal.innerText + eachButton.innerText)
+            };
+        });
+    };
+
+
+
+// for(let eachButton of allButtons){
+
+// }
